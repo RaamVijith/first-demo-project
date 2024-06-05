eeeaede86c81002611a47e5490c67b38f0fcfa1a
RaamVijith <raamvijith1@gmail.com>
2024-06-05 17:18:59 +0530

    demo

--- components/Common/DestinationsOne.js
+++ components/Common/DestinationsOne.js
@@ -9,7 +9,7 @@ const DestinationsOne = ({ titleOne,titleTwo,paddingBottom70,stateData }) => {
             <div className='section-title'>
               <h2>Popular States</h2>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                Lorem ipsum dolor sit asasasasmet, consectetur adipiscing elit, sed do
                 eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis
                 ipsum suspendisse ultrices gravida. Risus commodo viverra.
               </p>
